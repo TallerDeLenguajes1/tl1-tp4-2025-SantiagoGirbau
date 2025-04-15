f0d4c788702be9894b47d6ebea82baa9088e77ca
Santiago <Luisledesma2509@gmail.com>
2025-04-15 13:33:10 -0300

    Mostrar lista comenzado.

--- tp4_1.c
+++ tp4_1.c
@@ -3,61 +3,90 @@
 #include <stdio.h>
 #include <math.h>
 
-struct Tarea{
-int TareaID;//Numérico autoincremental comenzando en 1000
-char *Descripcion; //
-int Duracion; // entre 10 – 100
-}typedef tarea;
+struct Tarea
+{
+    int TareaID;       // Numérico autoincremental comenzando en 1000
+    char *Descripcion; //
+    int Duracion;      // entre 10 – 100
+} typedef tarea;
 
-struct Nodo{
-tarea T;
-struct Nodo *Siguiente;
-}typedef nodo;
+struct Nodo
+{
+    tarea T;
+    struct Nodo *Siguiente;
+} typedef nodo;
 
-
-typedef nodo* lista;
+typedef nodo *lista;
 
 lista crearLista(lista H);
 lista cargarTareas(lista L1);
+void mostrarLista(lista L);
 
 int main(int argc, char const *argv[])
 {
-    lista L1,L2;
-    L1=crearLista(L1);
-    L2=crearLista(L2);    
+    lista L1, L2;
+    L1 = crearLista(L1);
+    L2 = crearLista(L2);
+    int input;
+    do
+    {
+        printf("\n1: Cargar tarea  0: Terminar carga\n");
+        scanf("%i", &input);
+        if (input != 0)
+        {
+            L1 = cargarTareas(L1);
+        }
+    } while (input != 0);
 
-    L1=cargarTareas(L1);
-
+    mostrarLista(L1);
     return 0;
 }
 
-
-lista crearLista(lista H){
+lista crearLista(lista H)
+{
     return NULL;
 }
 
-lista cargarTareas(lista L1){
-    int input;
-    int index=1000;
-    char cargar[100];
+lista cargarTareas(lista L1)
+{
+
+    int index = 1000;
+    char tareaACargar[100];
     nodo *tareaNueva;
-    do
-    {
-        printf("\n1: Cargar tarea  0: Terminar carga.    ");
-        scanf("%i", &input);
-        if (input!=0)
-        {
-            tareaNueva=(nodo*)malloc(sizeof(nodo));
-            printf("\nEscriba la tarea a cargar:\n");
-            scanf("%s", cargar);
-            printf("\nEscriba la duracion de la tarea:\n");
-            scanf("%i", &tareaNueva->T.Duracion);
-            tareaNueva->T.Descripcion=(char*)malloc(sizeof(cargar+1));
-            strcpy(cargar,tareaNueva->T.Descripcion);
-            tareaNueva->T.TareaID=index;
-            tareaNueva->Siguiente=NULL;
-            index++;
-        }
-    } while (input!=0);
+
+    tareaNueva = (nodo *)malloc(sizeof(nodo));
+    printf("\nEscriba la tarea a cargar:\n");
+    scanf("%s", tareaACargar);
+
+    printf("\nEscriba la duracion de la tarea:\n");
+    scanf("%i", &tareaNueva->T.Duracion);
+
+    tareaNueva->T.Descripcion = (char *)malloc(strlen(tareaACargar + 1) * sizeof(char));
+    strcpy(tareaNueva->T.Descripcion, tareaACargar);
+
+    tareaNueva->T.TareaID = index;
+    tareaNueva->Siguiente = NULL;
+    
+    L1 = tareaNueva;
+    index++;
+
     return L1;
+}
+
+void mostrarLista(lista L)
+{
+    if (L == NULL)
+    {
+        printf("\nLa lista está vacía\n");
+    }
+    else
+    {
+        while (L != NULL)
+        {
+            printf("ID: %i \n", L->T.TareaID);
+            printf("Descripción: \n%s\n", L->T.Descripcion);
+            printf("Duración: %i\n", L->T.Duracion);
+            L = L->Siguiente;
+        }
+    }
 }
